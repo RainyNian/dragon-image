c31fa11266699bcc72b2b62e702f78b669abbc9b
Chase杨念 <7857257+chase_yang_nian@user.noreply.gitee.com>
2021-08-11 20:38:46 +0800

    uwu

--- pages/api/get.js
+++ pages/api/get.js
@@ -4,12 +4,13 @@ const fs = require('fs')
 const child_process = require('child_process')
 
 export default (req, res) => {
-  fs.readdir("public/image/", { encoding: 'utf8' }, (err, files) => {
+  var qwq = child_process.execSync("pwd && ls")
+  console.warn(qwq)
+  fs.readdir("/public/image/", { encoding: 'utf8' }, (err, files) => {
     if (err) throw err
     var image = files[Math.floor((Math.random() * files.length))]
-    var resp = "public/image/".concat(image)
-    var qwq = child_process.execSync("pwd && ls")
-    console.warn(qwq)
+    var resp = "/public/image/".concat(image)
+
     res.setHeader('Content-Type', 'image')
     res.status(200).send(fs.readFileSync(resp))
   })
