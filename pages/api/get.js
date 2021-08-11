9844b4789a53d5e0890a8fda6970b15747470446
Chase杨念 <7857257+chase_yang_nian@user.noreply.gitee.com>
2021-08-11 20:18:50 +0800

    uwu

--- pages/api/get.js
+++ pages/api/get.js
@@ -4,10 +4,10 @@ const fs = require('fs')
 
 
 export default (req, res) => {
-  fs.readdir("public/image", { encoding: 'utf8' }, (err, files) => {
+  fs.readdir("image", { encoding: 'utf8' }, (err, files) => {
     if (err) throw err
     var image = files[Math.floor((Math.random() * files.length))]
-    var resp = "public/image/".concat(image)
+    var resp = "image/".concat(image)
     res.setHeader('Content-Type', 'image')
     res.status(200).send(fs.readFileSync(resp))
   })
