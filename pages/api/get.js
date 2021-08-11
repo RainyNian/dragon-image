a5076a337d5204c3267eee35ac41de2311f28c0d
Chase杨念 <7857257+chase_yang_nian@user.noreply.gitee.com>
2021-08-11 20:11:37 +0800

    qwq

--- pages/api/get.js
+++ pages/api/get.js
@@ -1,13 +1,12 @@
 
-import Image from 'next/image'
+
 const fs = require('fs')
-// 异步读取目录内容
 
 
 export default (req, res) => {
   fs.readdir("public/image", { encoding: 'utf8' }, (err, files) => {
-    if (err)  throw err
-    var image = files[Math.floor((Math.random()*files.length))]
+    if (err) throw err
+    var image = files[Math.floor((Math.random() * files.length))]
     var resp = "public/image/".concat(image)
     res.setHeader('Content-Type', 'image')
     res.status(200).send(fs.readFileSync(resp))
