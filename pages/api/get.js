295428a0bcb6bd2a1b7be7b89429358f4857f128
Chase杨念 <7857257+chase_yang_nian@user.noreply.gitee.com>
2021-08-11 20:09:04 +0800

    uwu

--- pages/api/get.js
+++ pages/api/get.js
@@ -1,17 +1,19 @@
+
+import Image from 'next/image'
 const fs = require('fs')
-const path = require('path')
-let fileDir = path.resolve(__dirname, './')
 // 异步读取目录内容
-fs.readdir(fileDir, { encoding: 'utf8' }, (err, files) => {
+function readdir() {
+
+}
+
+export default (req, res) => {
+  var resp = readdir()
+  console.log(resp)
+  fs.readdir("public/image", { encoding: 'utf8' }, (err, files) => {
+    if (err)  throw err
     var image = files[Math.floor((Math.random()*files.length))]
-})
-var resp = "/iamge/".concat(image)
-function Get() {
-    return (
-        <Image
-        src={resp}
-      />
-    )
-  }
-  
-export default Get
+    var resp = "public/image/".concat(image)
+    res.setHeader('Content-Type', 'image')
+    res.status(200).send(fs.readFileSync(resp))
+  })
+}
